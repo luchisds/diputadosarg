257dd3653cda095356ece184ff84e858164a9fd0
Luciano Muñoz <luchisds@gmail.com>
2016-09-02 02:27:01 -0300

    config staticfiles

--- diputados/settings.py
+++ diputados/settings.py
@@ -127,3 +127,7 @@ STATICFILES_FINDERS = (
     'django.contrib.staticfiles.finders.FileSystemFinder',
     'django.contrib.staticfiles.finders.AppDirectoriesFinder',
 )
+
+STATICFILES_DIRS = (
+    'website/static/',
+)
